9fabbf8edb7fac3dad2329e8c4cc068bcfd5f5d1
Anton Laza <anton.laza.0116@gmail.com>
2022-04-29 17:41:39 +0200

    regex remove spec chars

--- Day2/main.js
+++ Day2/main.js
@@ -86,3 +86,6 @@ console.log(text3.substring(31, 55))
 let text_4 = "Love is the best thing in this world. Some found their love and some are still looking for their love."
 let pattern = /love/gi
 console.log(text_4.match(pattern))
+
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+console.log(sentence.replace(/[^a-zA-Z0-9 ]/g,''))
