627f50112c7145c1f4dc6b214a8c43043db6ac8a
Anton Laza <anton.laza.0116@gmail.com>
2022-04-29 18:04:14 +0200

    Day 2 LEVEL 3 done

--- Day2/main.js
+++ Day2/main.js
@@ -89,3 +89,9 @@ console.log(text_4.match(pattern))
 
 const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 console.log(sentence.replace(/[^a-zA-Z0-9 ]/g,''))
+
+let incomeText = "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month."
+income = incomeText.match(/\d+/gi)
+console.log(income)
+
+console.log(parseInt(income[0]))
