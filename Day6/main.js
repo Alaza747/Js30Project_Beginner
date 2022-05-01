1f8d23b6e4b7e3c53b364aeae9e2764548d7c8ac
Anton Laza <anton.laza.0116@gmail.com>
2022-05-01 13:08:14 +0200

    complete level2 exercises

--- Day6/main.js
+++ Day6/main.js
@@ -123,7 +123,7 @@ while(randArr.length < 5){
     }
 }
 console.log(randArr)
-*/
+
 
 let someText = ""
 let i = 0
@@ -133,3 +133,133 @@ while (i < 7){
     i++
 }
 console.log(someText)
+
+
+let numLetters = prompt("How many chars do you want?")
+let randCharStr = ""
+for (let i = 1; i <= numLetters; i++){
+    let randChar = Math.floor(Math.random() * (122-48) + 48).toString()
+    randChar = String.fromCharCode(randChar)
+    randCharStr += randChar
+}
+console.log(randCharStr)
+
+
+let randomColor = Math.floor(Math.random()*16777215).toString(16)
+console.log(randomColor)
+
+
+const rgbArr = []
+for(let i = 0; i < 3; i++){
+    let value = Math.floor(Math.random()* 255 + 1)
+    rgbArr.push(value)
+}
+console.log(`rgb(${rgbArr.toString(',')})`)
+
+
+const countriesUpper = []
+let i = 0
+while(i < countries.length){
+    countriesUpper.push(countries[i].toUpperCase())
+    i++
+}
+console.log(countriesUpper)
+
+
+const countriesLength = []
+let i = 0
+while (i < countries.length){
+    countriesLength[i] = countries[i].length
+    i++
+}
+console.log(countriesLength)
+
+
+const fullCountryArr = []
+let i = 0
+while (i < countries.length){
+    fullCountryArr[i] = [countries[i], countries[i].slice(0,3).toUpperCase(), countries[i].length]
+    i++
+}
+console.log(fullCountryArr)
+
+
+const landArr = []
+let i = 0
+while (i < countries.length){
+    if(countries[i].includes("land")) {
+        landArr.push(countries[i])
+    }
+    i++
+}
+console.log(landArr)
+
+
+const iaArr = []
+let i = 0
+while (i < countries.length){
+    if(countries[i].endsWith("ia")) {
+        iaArr.push(countries[i])
+    }
+    i++
+}
+console.log(iaArr)
+
+
+const charLengthArr = []
+let i = 0
+while (i < countries.length){
+    charLengthArr[i] = countries[i].length
+    i++
+}
+console.log(countries[charLengthArr.indexOf(Math.max(...charLengthArr))])
+
+
+const charLengthArr = []
+let i = 0
+while (i < countries.length){
+    if (countries[i].length == 5){
+        charLengthArr.push(countries[i])
+    }
+    i++
+}
+console.log(charLengthArr)
+
+
+const charLengthArr = []
+let i = 0
+while (i < webTechs.length){
+    charLengthArr[i] = webTechs[i].length
+    i++
+}
+console.log(webTechs[charLengthArr.indexOf(Math.max(...charLengthArr))])
+
+
+let mern = ""
+let i = 0
+
+while (i < mernStack.length){
+    mern += mernStack[i][0]
+    i++
+}
+console.log(mern)
+
+
+const fruit = ['banana', 'orange', 'mango', 'lemon']
+const fr = []
+for(let i = fruit.length-1; i >= 0; i--){
+    fr.push(fruit[i])
+}
+console.log(fr)
+
+
+const fullStack = [
+    ['HTML', 'CSS', 'JS', 'React'],
+    ['Node', 'Express', 'MongoDB']
+  ]
+let full = fullStack.join().split(',')
+for (let i = 0; i < full.length; i++){
+    console.log(`${full[i]}\n`)
+}
+*/
+
