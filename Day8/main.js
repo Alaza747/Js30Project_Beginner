7d46a4a2db1404b4fb94bf492101823e55ea595b
Anton Laza <anton.laza.0116@gmail.com>
2022-05-05 12:04:59 +0200

    Level 2 exercise 1 done

--- Day8/main.js
+++ Day8/main.js
@@ -1,4 +1,4 @@
-const dog = {
+/* const dog = {
     name: "Deb",
     legs: 4,
     color: "white",
@@ -14,3 +14,69 @@ dog.getDogInfo = function() {
 }
 
 console.log(dog.getDogInfo())
+
+*/
+
+const users = {
+    Alex: {
+      email: 'alex@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      age: 20,
+      isLoggedIn: false,
+      points: 30
+    },
+    Asab: {
+      email: 'asab@asab.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 25,
+      isLoggedIn: false,
+      points: 50
+    },
+    Brook: {
+      email: 'daniel@daniel.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+      age: 30,
+      isLoggedIn: true,
+      points: 50
+    },
+    Daniel: {
+      email: 'daniel@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    John: {
+      email: 'john@john.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+      age: 20,
+      isLoggedIn: true,
+      points: 50
+    },
+    Thomas: {
+      email: 'thomas@thomas.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    Paul: {
+      email: 'paul@paul.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    }
+  }
+
+
+function countSkills(x){
+    const skillArr = []
+    for (const person of Object.entries(x)){
+        skillArr.push(person[1].skills.length)
+    }
+    let indexHighSkill = skillArr.indexOf(Math.max(...skillArr)) 
+    return Object.keys(users)[indexHighSkill]
+}
+
+console.log(`The person with the highest number of skills is ${countSkills(users)}`)
