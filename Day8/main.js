d45cc5819aeed23c22453f7fe88dfbecc011a149
Anton Laza <anton.laza.0116@gmail.com>
2022-05-06 11:21:41 +0200

    Level 3 exercise 2a done

--- Day8/main.js
+++ Day8/main.js
@@ -115,4 +115,94 @@ function mernUsers(x){
 
 const anton = Object.assign({}, users)
 anton.Anton = {email: "HI", skills: ["HTML", "CSS", "JS"], age: 23}
-console.log(Object.values(anton))
+//console.log(Object.values(anton))
+
+const users1 = [
+    {
+        _id: 'ab12ex',
+        username: 'Alex',
+        email: 'alex@alex.com',
+        password: '123123',
+        createdAt:'08/01/2020 9:00 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'fg12cy',
+        username: 'Asab',
+        email: 'asab@asab.com',
+        password: '123456',
+        createdAt:'08/01/2020 9:30 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'zwf8md',
+        username: 'Brook',
+        email: 'brook@brook.com',
+        password: '123111',
+        createdAt:'08/01/2020 9:45 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'eefamr',
+        username: 'Martha',
+        email: 'martha@martha.com',
+        password: '123222',
+        createdAt:'08/01/2020 9:50 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'ghderc',
+        username: 'Thomas',
+        email: 'thomas@thomas.com',
+        password: '123333',
+        createdAt:'08/01/2020 10:00 AM',
+        isLoggedIn: false
+    }
+];
+
+const products = [
+  {
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+      { userId: 'fg12cy', rate: 5 },
+      { userId: 'zwf8md', rate: 4.5 }
+    ],
+    likes: []
+  },
+  {
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy']
+  },
+  {
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy']
+  }
+]
+
+
+function signUp (login) {
+    for(let i = 0; i < Object.keys(users1).length; i++) {
+        if(login == users1[0].username){
+            return "Is already registered."
+        }
+    }
+    users1.push({
+        id: "111111",
+        username: login, 
+        email: "login@gmail.com"
+    })
+    return "New User registered."
+}
+console.log(signUp("Anton"))
+console.log(users1)
