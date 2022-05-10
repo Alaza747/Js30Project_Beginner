465fa32478bc344d015b090dc1222cb54f3362c9
Anton Laza <anton.laza.0116@gmail.com>
2022-05-10 22:29:40 +0200

    1-5 done

--- Day10/main.js
+++ Day10/main.js
@@ -2,7 +2,7 @@
 /*
 // 1-1
 let a = new Set()
-*/
+
 // 1-2
 let a = [1,2,3,4,5,6,7,8,9,10]
 let b = new Set()
@@ -15,4 +15,9 @@ b.delete(5)
 
 // 1-4
 b.clear()
+*/
+
+// 1-5
+let c = ["a", "b", "c", "d", "e"]
+let b = new Set(c)
 console.log(b)
