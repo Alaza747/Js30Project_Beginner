298d6642cb95e9815673c686b308c8074d142ed8
Anton Laza <anton.laza.0116@gmail.com>
2022-05-12 23:26:19 +0200

    prepared 3-4

--- Day11/main.js
+++ Day11/main.js
@@ -87,7 +87,7 @@ for (const {name: a, scores: b,skills: c,age: d} of users){
 for (const {name, capital, population, languages} of countries_data){
     console.log(name, capital, population, languages)
 }
-*/
+
 
 // 3-2 
 const student = ['David', ['HTML', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
@@ -116,3 +116,26 @@ const convertArrayToObject = stud => {
 }
 
 convertArrayToObject(students)
+*/
+
+// 3-4 
+const student = {
+    name: 'David',
+    age: 25,
+    skills: {
+      frontEnd: [
+        { skill: 'HTML', level: 10 },
+        { skill: 'CSS', level: 8 },
+        { skill: 'JS', level: 8 },
+        { skill: 'React', level: 9 }
+      ],
+      backEnd: [
+        { skill: 'Node',level: 7 },
+        { skill: 'GraphQL', level: 8 },
+      ],
+      dataBase:[
+        { skill: 'MongoDB', level: 7.5 },
+      ],
+      dataScience:['Python', 'R', 'D3.js']
+    }
+  }
