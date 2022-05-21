6f65355d78dbf174eb1bca3ec3ad183c63080584
Anton Laza <anton.laza.0116@gmail.com>
2022-05-21 18:39:06 +0200

    1-3 postponed

--- Day12/main.js
+++ Day12/main.js
@@ -10,14 +10,15 @@ let income = "He earns 4000 euro from salary per month, 10000 euro annual bonus,
 let pattern = /\d+/g
 let result = income.match(pattern).map((a) => parseInt(a, 10)).reduce((sum, a) => sum += a)
 console.log(result)
-*/
+
 // 1-2
 let test = "first_name"
 let test2 = "first-name"
 let test3 = "1first_name"
 let test4 = "firstname"
-let pattern = /^[a-z]/
+let pattern = /^[a-z]*[\-]$/
 console.log(pattern.test(test))
 console.log(pattern.test(test2))
 console.log(pattern.test(test3))
 console.log(pattern.test(test4))
+*/
