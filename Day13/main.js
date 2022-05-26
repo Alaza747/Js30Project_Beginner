d7f6d5af19750303ffdf0539394a4adbcdc36c08
Anton Laza <anton.laza.0116@gmail.com>
2022-05-26 17:06:24 +0200

    learnt styling of console messages

--- Day13/main.js
+++ Day13/main.js
@@ -1,3 +1,15 @@
+//%c styling console messages
+console.log('%c30 Days Of JavaScript', 'color:green') // log output is green
+console.log(
+  '%c30 Days%c %cOf%c %cJavaScript',
+  'color:green',
+  '',
+  'color:red',
+  '',
+  'color:yellow'
+) // log output green red and yellow text
+
+
 //console.warn()
 
 console.warn('This is a warning')
