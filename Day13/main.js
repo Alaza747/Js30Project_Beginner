8e85fc5a05a1039f06d9f654e61f591e3876af6a
Anton Laza <anton.laza.0116@gmail.com>
2022-05-26 17:10:20 +0200

    console.table on different examples

--- Day13/main.js
+++ Day13/main.js
@@ -1,3 +1,5 @@
+/*
+
 //%c styling console messages
 console.log('%c30 Days Of JavaScript', 'color:green') // log output is green
 console.log(
@@ -24,7 +26,59 @@ console.error('This is an error message')
 console.error('We all make mistakes')
 
 
-//console.table()
+//console.table() on an Array
 
 const names = ['Asabeneh', 'Brook', 'David', 'John']
 console.table(names)
+
+*/
+
+// console.table() on an Object
+const user = {
+  name: 'Asabeneh',
+  title: 'Programmer',
+  country: 'Finland',
+  city: 'Helsinki',
+  age: 250
+}
+console.table(user)
+
+
+const countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo']
+  ]
+  console.table(countries)
+
+  const users = [
+    {
+      name: 'Asabeneh',
+      title: 'Programmer',
+      country: 'Finland',
+      city: 'Helsinki',
+      age: 250
+    },
+    {
+      name: 'Eyob',
+      title: 'Teacher',
+      country: 'Sweden',
+      city: 'London',
+      age: 25
+    },
+    {
+      name: 'Asab',
+      title: 'Instructor',
+      country: 'Norway',
+      city: 'Oslo',
+      age: 22
+    },
+    {
+      name: 'Matias',
+      title: 'Developer',
+      country: 'Denmark',
+      city: 'Copenhagen',
+      age: 28
+    }
+  ]
+  console.table(users)
