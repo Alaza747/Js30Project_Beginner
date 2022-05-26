56d8a46486141a30c022e11df320798feaa43e72
Anton Laza <anton.laza.0116@gmail.com>
2022-05-26 17:05:03 +0200

    learnt console.warn()

--- Day13/main.js
+++ Day13/main.js
@@ -1,3 +1,11 @@
+//console.warn()
+
+console.warn('This is a warning')
+console.warn(
+  'You are using React. Do not touch the DOM. Virtual DOM will take care of handling the DOM!'
+)
+console.warn('Warning is different from error')
+
 //console.table()
 
 const names = ['Asabeneh', 'Brook', 'David', 'John']
