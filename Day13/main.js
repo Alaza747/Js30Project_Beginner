89ed841439ab205b36e59133daefc80d00f1b7ec
Anton Laza <anton.laza.0116@gmail.com>
2022-05-26 17:50:03 +0200

    3-1 done

--- Day13/main.js
+++ Day13/main.js
@@ -223,10 +223,40 @@ console.table(countries_data)
 // Level 2
 //2-1
 console.assert(10>2*10, "That\'s not true.")
-*/
+
 
 //2-2
 console.warn("THIS IS A WARNING")
 
 //2-3
 console.error("EROROROROOR")
+*/
+
+// Level 3
+//3-1
+const some = [0,1,2,3,4,5,6,7,8,9]
+let i = 0
+console.time("time while loop")
+while(i < 10){
+    console.log(i)
+    i++
+}
+console.timeEnd("time while loop")
+
+console.time("time for loop")
+for(i = 0; i < 10; i++){
+    console.log(i)
+}
+console.timeEnd("time for loop")
+
+console.time("time foreach loop")
+some.forEach(element => {
+    console.log(element)
+});
+console.timeEnd("time foreach loop")
+
+console.time("time forof loop")
+for (const num of some) {
+    console.log(num)
+}
+console.timeEnd("time forof loop")
