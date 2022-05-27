bb556d150dfc86a17d38f1d83ec3988ddca950f7
Anton Laza <anton.laza.0116@gmail.com>
2022-05-27 10:04:28 +0200

    learnt getter and setter methods

--- Day15/main.js
+++ Day15/main.js
@@ -12,7 +12,8 @@ class Person {
         age = 18, 
         country = "randomCountry", 
         city = "randomCity",
-        score = 0
+        score = 0,
+        skills = []
     ) {
 //passing inputs to the class
         this.firstName = firstName
@@ -21,17 +22,39 @@ class Person {
         this.country = country
         this.city = city
         this.score = score
+        this.skills = skills
     }
 //creating a method inside a class
     getFullName() {
         const fullName = this.firstName + " " + this.lastName
         return fullName
     }
-
+//one more custom method 
     winAddScore(){
         this.score ++
         return this.score
     }
+
+//getter method
+    get getScore(){
+        return this.score
+    }
+
+//one more getter
+    get getSkills(){
+        return this.skills
+    }
+
+//setter method
+    set addPoints(points){
+        this.score += points
+    }
+
+//one more setter
+    set addSkill(skill){
+        this.skills.push(skill)
+    }
+
 }
 
 //initializing a class
@@ -40,4 +63,15 @@ const person = new Person("Anton", "Laza", 23, "AT", "VIE")
 console.log(person.getFullName())
 console.log(person)
 console.log(person.winAddScore())
-console.log(person.winAddScore())
+
+
+
+//getter method 
+console.log(person.getScore)
+console.log(person.getSkills)
+
+//setter method
+person.addPoints = 4
+person.addSkill = "JS"
+console.log(person.score)
+console.log(person.skills)
