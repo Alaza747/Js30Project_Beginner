3162a8b94b041b01c139bc2d2ab6925fae76a24b
Anton Laza <anton.laza.0116@gmail.com>
2022-06-04 18:46:05 +0200

    learnt how to set and get elements from storage

--- Day17/main.js
+++ Day17/main.js
@@ -1,16 +1,19 @@
 import { countries_data } from "./countries_data.js";
 
 let skills = [
-    { tech: 'HTML', level: 10 },
-    { tech: 'CSS', level: 9 },
-    { tech: 'JS', level: 8 },
-    { tech: 'React', level: 9 },
-    { tech: 'Redux', level: 10 },
-    { tech: 'Node', level: 8 },
-    { tech: 'MongoDB', level: 8 }
-  ]
-  
-  let skillJSON = JSON.stringify(skills)
-  localStorage.setItem('skills', skillJSON)
+{ tech: 'HTML', level: 10 },
+{ tech: 'CSS', level: 9 },
+{ tech: 'JS', level: 8 },
+{ tech: 'React', level: 9 },
+{ tech: 'Redux', level: 10 },
+{ tech: 'Node', level: 8 },
+{ tech: 'MongoDB', level: 8 }
+]
 
-  console.log(localStorage)
+//important to stringify first or it will be destructured
+let skillJSON = JSON.stringify(skills)
+localStorage.setItem('skills', skillJSON)
+localStorage.setItem('Age', 200)
+let html = localStorage.getItem('Age')
+
+console.log(html)
