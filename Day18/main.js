95f9143e418c6d5fbe2cefa5586cd31279f06147
Anton Laza <anton.laza.0116@gmail.com>
2022-06-16 16:47:20 +0200

    learnt callbacks

--- Day18/main.js
+++ Day18/main.js
@@ -1,1 +1,41 @@
 import { countries_data } from "./countries_data.js";
+
+// Callbacks
+
+const doSomething = (callback) => {
+    setTimeout(() => {
+        const skills = ['Html', 'Css', 'Js', 'React', 'Redux', 'Node', 'MongoDB'];
+        callback('Did not go well', skills);
+    }, 2000);
+}
+
+const callback = (error, data) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(data);
+    }
+}
+
+doSomething(callback);
+
+
+const doSomething2 = (callback) => {
+    setTimeout(() => {
+        const skills = ['Html', 'Css', 'Js', 'React', 'Redux', 'Node', 'MongoDB'];
+        callback(false, skills);
+    }, 2000);
+}
+
+const callback2 = (error, data) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(data);
+    }
+}
+
+doSomething2(callback2);
+
+
+// Promises
