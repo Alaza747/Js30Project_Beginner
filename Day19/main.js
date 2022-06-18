2f716d500a172263698dfd8c3c572569547972e6
Anton Laza <anton.laza.0116@gmail.com>
2022-06-18 16:35:33 +0200

    2-1 done

--- Day19/main.js
+++ Day19/main.js
@@ -17,5 +17,37 @@ function name () {
 }
 
 let some = name()
-console.log(some)
+//console.log(some)
 
+// 2-1 Create a closure which has three inner functions
+
+function person() {
+    let info = ''
+
+    function addName(surname){
+        info += surname
+        return info
+    }
+
+    function addFam(Fam){
+        info += ` ${Fam}`
+        return info
+    } 
+
+    function addAge(age){
+        info += ` is ${age} years old.`
+        return info 
+    }
+
+    return{
+        addName,
+        addFam,
+        addAge
+    }
+    
+}
+
+let Anton = person()
+console.log(Anton.addName('Anton'))
+console.log(Anton.addFam('Laza'))
+console.log(Anton.addAge(23))
