b67510441d6d0c2e49a654d060a9b6092c968cec
Anton Laza <anton.laza.0116@gmail.com>
2022-06-18 16:25:43 +0200

    1-1 done

--- Day19/main.js
+++ Day19/main.js
@@ -3,5 +3,19 @@ import { countries_data } from "./countries_data.js";
 
 // Level 1
 
-// 1-1 
+// 1-1 Create a closure which has one inner function
+
+
+function name () {
+    let name = "Anton"
+    function addName() {
+        name += " Laza"
+        return name
+    } 
+
+    return addName()
+}
+
+let some = name()
+console.log(some)
 
