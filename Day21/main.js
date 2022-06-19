f3fee18b5e313148de3ce5b557cf9f7b5e88c07b
Anton Laza <anton.laza.0116@gmail.com>
2022-06-19 14:45:16 +0200

    1-1 done

--- Day21/main.js
+++ Day21/main.js
@@ -1,1 +1,16 @@
 import { countries_data } from "./countries_data.js";
+
+// Level 1
+// 1-1 Create an index.html file and put four p elements as above: Get the first paragraph by using document.querySelector(tagname) and tag name
+
+const getP = document.getElementsByTagName('p')
+
+
+for (let i in getP){
+    console.log(getP[i])
+}
+
+
+getP.forEach(element => {
+    console.log(element)
+});
